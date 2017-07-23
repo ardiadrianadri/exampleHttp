c405a3885efb8a31f104c0db2d57c5a217f7bb65
Adrian Ferreres <ardiadrianadri@gmail.com>
2017-07-23 19:49:01 +0200

    Solution 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SuperHeroService } from './superHero.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  heroName = '';
+  listHeros: string[];
+  loading = false;
+
+  constructor (private _superHeroService: SuperHeroService) {}
+
+  lookforHero () {
+    this.loading = true;
+    this._superHeroService.getHeroes(this.heroName)
+    .subscribe(
+      data => { 
+        this.listHeros = data; 
+        this.loading = false;
+      },
+      error => { 
+        console.error (error);
+        this.loading = false;
+      }
+    );
+  }
 }
