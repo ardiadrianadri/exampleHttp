e4dae35ec43b4338423d2d37fb27152339d19da2
Adrian Ferreres <ardiadrianadri@gmail.com>
2017-07-26 21:27:05 +0200

    Second solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SuperHeroService } from './superHero.service';
 
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,23 +10,12 @@ import { SuperHeroService } from './superHero.service';
 })
 export class AppComponent {
   heroName = '';
-  listHeros: string[];
+  listHeros: Observable <string[]>;
   loading = false;
 
   constructor (private _superHeroService: SuperHeroService) {}
 
   lookforHero () {
-    this.loading = true;
-    this._superHeroService.getHeroes(this.heroName)
-    .subscribe(
-      data => { 
-        this.listHeros = data; 
-        this.loading = false;
-      },
-      error => { 
-        console.error (error);
-        this.loading = false;
-      }
-    );
+    this.listHeros = this._superHeroService.getHeroes(this.heroName);
   }
 }
